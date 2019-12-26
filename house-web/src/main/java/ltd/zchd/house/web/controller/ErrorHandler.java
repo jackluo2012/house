42f5a0dcdb093eef1851b422286782257970c785
jackluo <net.webjoy@gmail.com>
2019-12-26 14:46:21 +0800

    设置错误请求页面

---
+++ house-web/src/main/java/ltd/zchd/house/web/controller/ErrorHandler.java
@@ -0,0 +1,21 @@
+package ltd.zchd.house.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    private  static  final Logger logger = LoggerFactory.getLogger(ErrorHandler.class);
+
+    @ExceptionHandler(value = {Exception.class,RuntimeException.class})
+    public String error500(HttpServletRequest request,Exception e){
+        logger.error(e.getMessage());
+        logger.error(request.getRequestURL()+ "encounter 500");
+        return "error/500";
+    }
+}
